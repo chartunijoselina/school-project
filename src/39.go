06520a517c2bb98ea6f1071928acce7a60ed451e
chartunijoselina <chartunijoselina@gmail.com>
2025-05-25 00:32:52 +0000

    Add 39.go

---
+++ src/39.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Example code snippet demonstrating basic Go programming concepts
+    var x int = 10
+    if x > 5 {
+        fmt.Println("x is greater than 5")
+    } else {
+        fmt.Println("x is not greater than 5")
+    }
+}
